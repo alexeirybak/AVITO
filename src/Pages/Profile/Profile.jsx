151aeb1772bb2b6612284fd3f390556c59490b70
alexeirybak <rybak2108@yandex.ru>
2023-12-30 00:39:07 +0300

    Part 2/2

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -3,10 +3,46 @@ import { ReturnToMain } from '../../Components/ReturnToMain.js/ReturnToMain';
 import { Card } from '../../Components/Card/Card';
 import { Footer } from '../../Components/Footer/Footer';
 import * as S from './Profile.styled';
-import { getUser } from '../../Api/userApi';
+import { useChangeMeMutation, useGetMeQuery } from '../../Store/RTKQuery/getMe';
+import { getAccessTokenLocal } from '../../helpers/token';
+import { updateToken } from '../../Api/tokenApi';
+import { useEffect, useState } from 'react';
+import { handleAvatarClick, handleAvatarUpload, handleChangeMe, profileUserData, saveUserLocal } from '../../helpers/user';
+import { useGetAllMyAdsQuery } from '../../Store/RTKQuery/getMyAds';
 
-export const Profile = ({ products }) => {
-  getUser()
+export const Profile = ({}) => {
+  const [city, setCity] = useState('')
+  const [avatar, setAvatar] = useState(null)
+  const [userName, setUserName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [surname, setSurname] = useState('')
+  const [img, setImg] = useState(null);
+  const access = getAccessTokenLocal()
+  const fileUpload = document.getElementById("file-upload");
+  const {data =[], isError, error, isSuccess, refetch} = useGetMeQuery(access);
+  const [changeMe, {isError: isErrorChangeMe, error: errorChangeMe}] = useChangeMeMutation()
+  const {data: dataMyAds=[]} = useGetAllMyAdsQuery(access)
+  const asyncUpgate = async () => {
+    await updateToken()
+    await refetch()
+    return
+  }
+  useEffect(() => {
+    if(isSuccess) {
+      const response = data
+      saveUserLocal(response.email, response.name, response.id)
+      profileUserData(data, setUserName, setSurname, setCity, setPhone, setAvatar)
+    }
+    if(isError && error.status === 401 && error.data.detail === "Could not validate credentials: Not enough segments") {
+      asyncUpgate()
+      return
+    }
+    if(isError && error.status === 401 && error.data.detail === 'Could not validate credentials: Signature has expired') {
+      asyncUpgate()
+      return
+    }
+  }, [isSuccess, isError])
+
   return (
     <S.Wrapper>
       <S.Container>
@@ -15,18 +51,31 @@ export const Profile = ({ products }) => {
           <S.MainContainer>
             <S.MainCenterBlock>
               <ReturnToMain />
-              <S.MainH2>Здравствуйте, Антон!</S.MainH2>
+              <S.MainH2>{userName === '' ? "Здравствуйте, Неизвестный!" : `Здравствуйте, ${userName}!`}</S.MainH2>
               <S.MainProfile>
                 <S.ProfileContent>
                   <S.ProfileTitle>Настройки профиля</S.ProfileTitle>
                   <S.ProfileSettings>
                     <S.SettingsLeft>
                       <S.SettingsImg>
-                        <S.SettingsImgLink href='#' target='_self'>
-                          <S.SettingsImgImg src='img/empty-profile.svg' />
+                        <S.SettingsImgLink >
+                          <S.SettingsImgImg src={avatar === null ? 'img/empty-profile.svg' : `http://localhost:8090/${avatar}`} alt='first_photo'/>
                         </S.SettingsImgLink>
                       </S.SettingsImg>
-                      <S.SettingsChangePhoto href='#' target='_self'>
+                      <S.SettingsImgInput
+                        id="file-upload"
+                        type="file"
+                        accept="image/*"
+                        onChange={(event) => {
+                          event.preventDefault();
+                          const file = event.target.files?.[0];
+                          if (file) {
+                            setImg(file);
+                            handleAvatarUpload(file, setAvatar, refetch);
+                          }
+                        }}
+                      ></S.SettingsImgInput>
+                      <S.SettingsChangePhoto onClick={() => handleAvatarClick(event, fileUpload, setAvatar)}>
                         Заменить
                       </S.SettingsChangePhoto>
                     </S.SettingsLeft>
@@ -42,6 +91,8 @@ export const Profile = ({ products }) => {
                               name='fname'
                               type='text'
                               placeholder=''
+                              value={userName}
+                              onChange={e => setUserName(e.target.value)}
                             />
                           </S.SettingsDiv>
 
@@ -53,6 +104,8 @@ export const Profile = ({ products }) => {
                               name='lname'
                               type='text'
                               placeholder=''
+                              value={surname}
+                              onChange={e => setSurname(e.target.value)}
                             />
                           </S.SettingsDiv>
                         </S.SettingsNameBox>
@@ -64,6 +117,8 @@ export const Profile = ({ products }) => {
                             name='city'
                             type='text'
                             placeholder=''
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
                           />
                         </S.SettingsDiv>
 
@@ -75,10 +130,12 @@ export const Profile = ({ products }) => {
                             name='phone'
                             type='tel'
                             placeholder='+...'
+                            value={phone}
+                            onChange={e => setPhone(e.target.value)}
                           />
                         </S.SettingsDiv>
 
-                        <S.SettingsButton>Сохранить</S.SettingsButton>
+                        <S.SettingsButton onClick={() => handleChangeMe(access, userName, surname, phone, city, changeMe)}>Сохранить</S.SettingsButton>
                       </S.SettingsForm>
                     </S.SettingsRight>
                   </S.ProfileSettings>
@@ -88,7 +145,7 @@ export const Profile = ({ products }) => {
             </S.MainCenterBlock>
             <S.MainContent>
               <S.ContentCards>
-                {products.map((product) => (
+                {dataMyAds.map((product) => (
                   <Card key={product.id} product={product} />
                 ))}
               </S.ContentCards>
